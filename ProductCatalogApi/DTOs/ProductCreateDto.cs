f134c6cf7ef3d5e216db9d25e83a59a2bbcaa3db
Abdalle <abdalle-abdulkadir@chasacademy.se>
2026-03-18 20:19:08 +0100

    Add validation to productCreateDto

--- ProductCatalogApi/DTOs/ProductCreateDto.cs
+++ ProductCatalogApi/DTOs/ProductCreateDto.cs
@@ -4,13 +4,13 @@ namespace ProductCatalogApi.DTOs
 {
     public class ProductCreateDto
     {
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
-        [Range(1, 100000)]
+        [Range(1, 100000, ErrorMessage = "Price must be between 1 and 100000")]
         public decimal Price { get; set; }
 
         public int CategoryId { get; set; }
