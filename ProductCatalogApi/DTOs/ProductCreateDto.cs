99919925f9184fd4e169f8d34ac1298b891d795c
Abdalle <abdalle-abdulkadir@chasacademy.se>
2026-03-22 21:23:38 +0100

    Finalize project

--- ProductCatalogApi/DTOs/ProductCreateDto.cs
+++ ProductCatalogApi/DTOs/ProductCreateDto.cs
@@ -13,8 +13,12 @@ namespace ProductCatalogApi.DTOs
         [Range(1, 100000, ErrorMessage = "Price must be between 1 and 100000")]
         public decimal Price { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "CategoryId must be valid")]
         public int CategoryId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "SupplierId must be valid")]
         public int SupplierId { get; set; }
+
     }
 }
 
